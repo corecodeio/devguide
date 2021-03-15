6e89f05efabc87b7213781bafc8d7a8b415825bc
Yosef Maldonado Arriaga <yosefmarr@gmail.com>
2021-03-15 16:26:54 -0600

    exercises 1.1 java

---
+++ src/technologies/2021/java/week1/exercises/src/E0/E0.java
@@ -0,0 +1,17 @@
+Public class test
+{
+    public Static void main(String args)
+    {
+        integer[] numbers = [0,1,2,3,4,5];
+        for(int i; i < numbers.lenght ; i++)
+        {
+            system.out.printl(numbers[i]+'\n');
+        }
+        printMessage("You fix the errors!")
+    }
+
+    public void printMessage(String mssg)
+    {
+        System.out.println(msg)
+    }
+}
